3db7ea7539455fa4c6bf0ce84e0c0464bd037107
Sam Heinz <54530346+asylumexp@users.noreply.github.com>
2025-10-08 09:18:54 +1000

    script(stylus): use arm64 singlefile

--- ct/stylus.sh
+++ ct/stylus.sh
@@ -34,7 +34,7 @@ function update_script() {
     systemctl stop stylus
     msg_ok "Stopped $APP"
 
-    fetch_and_deploy_gh_release "stylus" "mmastrac/stylus" "singlefile" "latest" "/usr/bin/" "*_linux_amd64"
+    fetch_and_deploy_gh_release "stylus" "mmastrac/stylus" "singlefile" "latest" "/usr/bin/" "*_linux_arm64"
 
     msg_info "Starting $APP"
     systemctl start stylus
